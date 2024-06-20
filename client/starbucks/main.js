06fcc001e38ce6fcb35c687ea5b7f4f2ee55ed72
JoowonJang <wndnjs5248@hanmail.net>
2024-06-20 10:33:50 +0900

    starbucks menu
    - normal version

---
+++ client/starbucks/main.js
@@ -0,0 +1,20 @@
+const aList = document.querySelectorAll('nav a');
+const depthList = document.querySelectorAll('.depth');
+const header = document.querySelector('#header');
+
+const h = (t) => (t.style.height = 0);
+
+aList.forEach((a) => {
+  a.addEventListener('mouseenter', () => {
+    const target = a.lastElementChild;
+
+    // h(t) -> h
+    depthList.forEach(h);
+
+    target.style.height = '100px';
+  });
+
+  a.addEventListener('mouseleave', () => depthList.forEach(h));
+});
+
+// header.addEventListener('mouseleave', () => depthList.forEach(h));
