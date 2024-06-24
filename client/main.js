c7773923a19e74a9bb86b6c3a58b9a4bc79e6613
JoowonJang <wndnjs5248@hanmail.net>
2024-06-24 15:15:31 +0900

    learn. calculator

--- client/main.js
+++ client/main.js
@@ -1,3 +1,56 @@
-console.log('안녕 웹브라우저!');
+import { 
+  attr, 
+  getNode, 
+  clearContents, 
+  insertLast 
+} from "./lib/index.js";
 
-const a = 'hello';
+function phase2() {
+  const calculator = getNode('.calculator');
+  const result = getNode('.result');
+  const clear = getNode('#clear');
+  const numberInputs = [...document.querySelectorAll('input:not(#clear)')];
+
+  function handleInput() {
+    const total = numberInputs.reduce((acc, cur) => acc + Number(cur.value), 0);
+
+    clearContents(result);
+    insertLast(result, total);
+  }
+
+  function handleClear(e) {
+    e.preventDefault();
+    numberInputs.forEach(clearContents);
+    result.textContent = '-';
+  }
+
+  calculator.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
+}
+
+const first = getNode('#firstNumber');
+const second = getNode('#secondNumber');
+const result = getNode('.result');
+const clear = getNode('#clear');
+
+function handleInput() {
+  const firstValue = Number(first.value);
+  const secondValue = +second.value;
+  const total = firstValue + secondValue;
+
+  clearContents(result);
+
+  insertLast(result, total);
+}
+
+function handleClear(e) {
+  e.preventDefault();
+
+  clearContents(first);
+  clearContents(second);
+  result.textContent = '-';
+}
+
+first.addEventListener('input', handleInput);
+second.addEventListener('input', handleInput);
+clear.addEventListener('click', handleClear);
