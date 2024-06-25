0f6a1c0f68be438bcfa8280bc3dc89db6a10a2dc
JoowonJang <wndnjs5248@hanmail.net>
2024-06-25 11:39:24 +0900

    learn. randomDice

--- client/main.js
+++ client/main.js
@@ -1,45 +1,98 @@
-import data from './data/data.js';
-
-import { getNode, getRandom, addClass, removeClass, showAlert, isNumericString, shake, copy } from './lib/index.js';
-
-const submit = getNode('#submit');
-const nameField = document.querySelector('#nameField');
-const result = document.querySelector('.result');
 
 
-function handleSubmit(e) {
-  e.preventDefault();
+import { 
+  attr,
+  getNode,
+  getNodes,
+  endScroll,
+  insertLast,
+  diceAnimation,
+  clearContents,
+ } from "./lib/index.js";
 
-  const name = nameField.value;
-  const list = data(name);
-  const pick = list[getRandom(list.length)];
 
-  if(!name || name.replace(/\s*/g,'') === '') {
-    showAlert('.alert-error','공백은 허용하지 않습니다.');
+// 1. 주사위 애니메이션
+// 2. 주사위 굴리기 버튼을 클릭하면 diceAnimation() 실행될 수 있도록
 
-    shake('#nameField').restart();
-    
-    return;
-  }
-  if(!isNumericString(name)) {
-    showAlert('.alert-error','제대로된 이름을 입력해 주세요.');
 
-    shake('#nameField').restart();
 
-    return;
-  }
 
-  result.textContent = pick;
+const [rollingButton,recordButton,resetButton] = getNodes('.buttonGroup > button');
+const recordListWrapper = getNode('.recordListWrapper');
+
+
+let count = 0;
+let total = 0;
+
+
+function createItem(value){
+  const template = `
+    <tr>
+      <td>${++count}</td>
+      <td>${value}</td>
+      <td>${total += value}</td>
+    </tr>
+  `
+  return template
 }
 
-function handleCopy() {
-  const text = result.textContent;
-  if(nameField.value) {
-    copy(text).then(() => {
-      showAlert('.alert-success','클립보드 복사 완료!');
-    })
-  }
+
+function renderRecordItem(){
+  
+  // const diceValue = getNode('#cube').getAttribute('dice');
+  const diceValue = Number(attr(getNode('#cube'),'dice'));
+
+
+  insertLast('.recordList tbody',createItem(diceValue))
+  
+  // 1. insertLast 함수 사용
+
+  // 2. template 전달
+
+  // 3. diceValue interpolation(보간법) 하기 ex) <td>${diceValue}</td>
+  endScroll(recordListWrapper);
+
 }
 
-submit.addEventListener('click', handleSubmit);
-result.addEventListener('click',handleCopy);
+
+
+const handleRollingDice = (() => {
+
+  
+  let isClicked = false;
+  let stopAnimation;
+
+  return ()=>{
+    if(!isClicked){
+      stopAnimation  = setInterval(diceAnimation, 100);
+      recordButton.disabled = true;
+      resetButton.disabled = true;
+    }
+    else{
+      clearInterval(stopAnimation);
+      recordButton.disabled = false;
+      resetButton.disabled = false;
+    }
+    isClicked = !isClicked;
+  }
+})()
+
+
+function handleRecord(){
+  recordListWrapper.hidden = false;
+
+  renderRecordItem()
+}
+
+
+function handleReset(){
+  recordListWrapper.hidden = true;
+  clearContents('tbody');
+  count = 0;
+  total = 0;
+  
+}
+
+rollingButton.addEventListener('click',handleRollingDice)
+recordButton.addEventListener('click',handleRecord)
+resetButton.addEventListener('click',handleReset)
